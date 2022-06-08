56a15fc438360f3046229bffd009bc34a50e8804
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2022-06-08 19:31:18 +0200

    Finalized push e2e tests

--- e2e/03push.test.ts
+++ e2e/03push.test.ts
@@ -1,20 +1,27 @@
 import {spawnSync} from 'child_process';
-import * as fs from 'fs';
 import {readMultiClaspConfig} from '../src/common';
 import {E2E_PROJECTS_NAMES, MULTI_CLASP_PATHS, NODE} from './constants';
 
-describe('Generate the empty projects', () => {
+describe('Test PUSH command', () => {
 
   beforeAll(() => {
     expect(readMultiClaspConfig()).not.toEqual([]);
   });
 
-  it.each(E2E_PROJECTS_NAMES)('Pushing project %s', (projectName)=>{
+  it.each(E2E_PROJECTS_NAMES)('Running PUSH for project %s', ()=>{
     expect(true).toBe(true);
     const result = spawnSync(NODE, [MULTI_CLASP_PATHS.MULTI_CLASP, "push"], {
       encoding: 'utf8',
     });
-    console.log('result', result);
+    expect(result.stdout).toContain('Pushed 3 files.');
+    expect(result.status).toBe(0);
+  });
+
+  it.each(E2E_PROJECTS_NAMES)('Running PUSH with "--force" for project %s', ()=>{
+    expect(true).toBe(true);
+    const result = spawnSync(NODE, [MULTI_CLASP_PATHS.MULTI_CLASP, "push", "--force"], {
+      encoding: 'utf8',
+    });
     expect(result.stdout).toContain('Pushed 3 files.');
     expect(result.status).toBe(0);
   });
