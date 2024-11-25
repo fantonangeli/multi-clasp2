760d5460de16a41760ab8d9cf06ba3d81ccf8cc6
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2024-11-25 19:55:02 +0100

    Update constants.ts

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -1,16 +1,16 @@
 import * as os from 'os';
+import * as path from 'path';
 
 export const NODE = 'node';
 export const CLASP: string = os.type() === 'Windows_NT' ? 'clasp.cmd' : 'clasp';
 
-const BUILD_PATH = 'build/src/';
-const SCRIPT_SRC_PATH = "./scripts_src";
+const BUILD_PATH = path.join('build', 'src');
+const SCRIPT_SRC_PATH = path.join('.', 'scripts_src');
 
 // Paths
 export const MULTI_CLASP_PATHS = {
   SCRIPT_SRC: SCRIPT_SRC_PATH,
-  MULTI_CLASP: BUILD_PATH + "index.js",
+  MULTI_CLASP: path.join(BUILD_PATH, 'index.js'),
 };
 
-export const E2E_PROJECTS_NAMES = ["multi-clasp2-e2e-1" , "multi-clasp2-e2e-2" ];
-
+export const E2E_PROJECTS_NAMES = ['multi-clasp2-e2e-1', 'multi-clasp2-e2e-2'];
